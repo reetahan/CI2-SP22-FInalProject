7cdd9d995b091e1f23468537dd66519f867a61cc
Reetahan <rm10@illinois.edu>
2022-05-05 21:50:07 -0400

    updated graph parser

--- src/graph_parsing.py
+++ src/graph_parsing.py
@@ -4,11 +4,14 @@ from collections import namedtuple
 import numpy as np
 import pandas as pd
 import tensorflow as tf
+from time import sleep
+import time 
+
 
 from relational_erm.graph_ops.representations import PackedAdjacencyList
 from relational_erm.graph_ops.representations import create_packed_adjacency_from_redundant_edge_list
 
-def _load_profiles(profile_file):
+def load_profiles(profile_file):
 
     names = str.split("user_id public completion_percentage gender region last_login registration age body "
                       "I_am_working_in_field spoken_languages hobbies I_most_enjoy_good_food pets body_type "
@@ -20,61 +23,53 @@ def _load_profiles(profile_file):
                       "my_active_sports my_passive_sports profession I_like_books life_style music cars politics "
                       "relationships art_culture hobbies_interests science_technologies computers_internet education "
                       "sport movies travelling health companies_brands more")
-
-    usecols = str.split("user_id public completion_percentage gender region last_login registration age")
-
-    extracols = str.split("completed_level_of_education " 
-                          "sign_in_zodiac "
-                          "relation_to_casual_sex " 
-                          "I_like_books " 
-                          )
+    usecols = str.split("user_id public completion_percentage gender region last_login registration age "
+                        "completed_level_of_education sign_in_zodiac relation_to_casual_sex I_like_books ")
 
 
-    usecols = usecols + extracols
-
-    profiles = pd.read_csv(profile_file, names=names, index_col=False, usecols=usecols, compression='gzip', header=None, sep='\t')
+    profiles = pd.read_csv(profile_file, names=names, index_col=False, usecols=usecols, header=None, sep='\t')
     profiles.set_index('user_id', inplace=True, drop=False)
+
+    profiles['region'] = profiles['region'].astype('category')
+    profiles['public'] = profiles['public'].astype('category')
+    profiles['gender'] = profiles['gender'].astype('category')
+    profiles.loc[profiles.age == 0, 'age'] = np.nan
+
+    profiles['completed_level_of_education'] = profiles['completed_level_of_education'].isna()
+    profiles['sign_in_zodiac'] = profiles['sign_in_zodiac'].isna()
+    profiles['relation_to_casual_sex'] = profiles['relation_to_casual_sex'].isna()
+    profiles['I_like_books'] = profiles['I_like_books'].isna()
+
+    profiles['last_login'] = pd.to_datetime(profiles['last_login'])
+    profiles['registration'] = pd.to_datetime(profiles['registration'])
+    profiles['user_id'] = profiles['user_id'] - 1
+
     return profiles
 
 
-def _process_profiles(profiles):
-
-    p2 = profiles
-    p2['region'] = p2['region'].astype('category')
-    p2['public'] = p2['public'].astype('category')
-    p2['gender'] = p2['gender'].astype('category')
-    p2.loc[p2.age == 0, 'age'] = np.nan
-
-    p2['completed_level_of_education'] = p2['completed_level_of_education'].isna()
-    p2['sign_in_zodiac'] = p2['sign_in_zodiac'].isna()
-    p2['relation_to_casual_sex'] = p2['relation_to_casual_sex'].isna()
-    p2['I_like_books'] = p2['I_like_books'].isna()
-
-    p2['last_login'] = pd.to_datetime(p2['last_login'])
-    p2['registration'] = pd.to_datetime(p2['registration'])
-
-    return p2
-
-
 def preprocess_data():
-    link_file = os.path.join(data_directory, 'soc-pokec-relationships.txt.gz')
-    profile_file = os.path.join(data_directory, 'soc-pokec-profiles.txt.gz')
+    link_file = '../data/soc-pokec-relationships.txt'
+    profile_file = '../data/soc-pokec-profiles.txt'
 
+    print('Loading Relationship File')
+    start = time.time()
     edge_list = np.loadtxt(link_file, dtype=np.int32)
-    profiles = _load_profiles(profile_file)
-    profiles = _process_profiles(profiles)
-
-    profiles['user_id'] = profiles['user_id'] - 1
     edge_list = edge_list - 1
+    end = time.time()
+    print('Completed Relationship Loading, Time taken was: ' + str(end - start))
 
-    return {
-        'edge_list': edge_list,
-        'profiles': profiles
-    }
+    print('Loading Profiles File')
+    start = time.time()
+    profiles = load_profiles(profile_file)
+    end = time.time()
+    print('Completed Profiles Loading, Time taken was: ' + str(end-start))
+    
+    return edge_list, profiles
 
 
 def preprocess_packed_adjacency_list(edge_list):
 
+    orig_edge_list = edge_list.copy()
     edge_list = edge_list[edge_list[:, 0] != edge_list[:, 1], :]
     edge_list.sort(axis=-1)
     edge_list = np.unique(edge_list, axis=0)
@@ -86,7 +81,8 @@ def preprocess_packed_adjacency_list(edge_list):
         'neighbours': adjacency_list.neighbours,
         'offsets': adjacency_list.offsets,
         'lengths': adjacency_list.lengths,
-        'vertex_index': adjacency_list.vertex_index
+        'vertex_index': adjacency_list.vertex_index,
+        'edge_list': orig_edge_list
     }
 
 
@@ -131,56 +127,10 @@ def subset_to_region(edge_list, profiles, regions=None):
 
     edge_list = user_id_to_index[edge_list]
 
-    regional_profiles.to_pickle("regional_profiles.pkl")
-    np.savez_compressed('regional_pokec_links.npz', edge_list=edge_list)
+    regional_profiles.to_pickle("../data/regional_profiles.pkl")
+    #np.savez_compressed('regional_links.npz', edge_list=edge_list)
     packed_adjacency_list_data = preprocess_packed_adjacency_list(edge_list)
-    np.savez_compressed('regional_pokec_links_processed.npz', **packed_adjacency_list_data)
-
-
-GraphData = namedtuple('GraphData', ['edge_list',
-                                     'weights',
-                                     'adjacency_list',
-                                     'num_vertices'])
-
-
-def load_data_pokec(data_folder=None):
-    if data_folder is None:
-        data_folder = 'dat/pokec/regional_subset'
-
-
-    data_path = os.path.join(data_folder, 'pokec_links.npz')
-    with tf.io.gfile.GFile(data_path, mode='rb') as f:
-        loaded = np.load(f, allow_pickle=False)
-
-    edge_list = loaded['edge_list'].astype(np.int32)
-
-    if 'weights' in loaded:
-        weights = loaded['weights'].astype(np.float32)
-    else:
-        weights = np.ones(edge_list.shape[0], dtype=np.float32)
-    
-    data_path = os.path.join(data_folder, 'pokec_links_processed.npz')
-    with tf.io.gfile.GFile(data_path, mode='rb') as f:
-        loaded = np.load(f, allow_pickle=False)
-    neighbours = loaded['neighbours']
-    offsets = loaded['offsets']
-    lengths = loaded['lengths']
-    vertex_index = loaded['vertex_index']
-
-
-    adjacency_list = PackedAdjacencyList(neighbours, weights, offsets, lengths, vertex_index)
-    graph_data = GraphData(edge_list=edge_list,
-                           weights=weights,
-                           adjacency_list=adjacency_list,
-                           num_vertices=len(vertex_index))
-
-    profile_file = os.path.join(data_folder, 'profiles.pkl')
-    profiles = pd.read_pickle(profile_file)
-
-    return graph_data, profiles
-
-def _standardize(x):
-    return (x-x.mean())/x.std()
+    np.savez_compressed('../data/regional_links.npz', **packed_adjacency_list_data)
 
 
 def process_pokec_attributes(profiles):
@@ -204,9 +154,9 @@ def process_pokec_attributes(profiles):
     profiles['old_school'] = (profiles['registration'] < pd.Timestamp(2009, 1, 1))
 
     profiles['scaled_registration'] = (profiles['registration'] - profiles['registration'].min()) / pd.offsets.Day(1)
-    profiles['scaled_registration'] = _standardize(profiles['scaled_registration'])
+    profiles['scaled_registration'] = (profiles['scaled_registration'] - profiles['scaled_registration'].mean())/(profiles['scaled_registration'].std())
 
-    profiles['scaled_age'] = _standardize(profiles['age'])
+    profiles['scaled_age'] = (profiles['age'] - profiles['age'].mean())/(profiles['age'].std())
 
 
     cat_columns = profiles.select_dtypes(['category']).columns
@@ -225,23 +175,3 @@ def process_pokec_attributes(profiles):
         pokec_features[feature] = profiles[feature].values
 
     return pokec_features
-
-
-def main():
-    
-    data = preprocess_data()
-
-    data['profiles'].to_pickle("profiles.pkl")
-    np.savez_compressed('pokec_links.npz', edge_list=data['edge_list'])
-
-    packed_adjacency_list_data = preprocess_packed_adjacency_list(data['edge_list'])
-    np.savez_compressed('pokec_links_processed.npz', **packed_adjacency_list_data)
-
-    data_folder = os.getcwd()
-
-    graph_data, profiles = load_data_pokec(data_folder)
-    subset_to_region(graph_data.edge_list, profiles, regions=None)
-
-
-if __name__ == '__main__':
-    main()
--- src/main.py
+++ src/main.py
@@ -1,13 +1,43 @@
 import numpy as np
+from graph_parsing import *
+from time import sleep
 
 def sim():
     pass
 
-def load_fb_graph():
-    pass
+def one_time_graph_processing():
+    edges, profiles = preprocess_data()
+    profiles.to_pickle("../data/profiles.pkl")
+    packed_adjacency_list_data = preprocess_packed_adjacency_list(edges)
+    np.savez_compressed('../data/links_processed.npz', **packed_adjacency_list_data)
+    subset_to_region(edges, profiles, regions=None)
+
+def graph_processing(regional):
+    profile_file = "../data/profiles.pkl"
+    edge_file = "../data/links.npz"
+
+    if(regional):
+        profile_file = "../data/regional_profiles.pkl"
+        edge_file = "../data/regional_links.npz"
+
+    profiles = pd.read_pickle(profile_file)
+
+    loaded = np.load(edge_file, allow_pickle=False)
+    neighbours = loaded['neighbours']
+    offsets = loaded['offsets']
+    edge_list = loaded['edge_list']
+    lengths = loaded['lengths']
+    vertex_index = loaded['vertex_index']
+    weights = np.ones(edge_list.shape[0], dtype=np.float32)
+    adjacency_list = PackedAdjacencyList(neighbours, weights, offsets, lengths, vertex_index)
+    graph = {"edge_list":edge_list,"weights":weights,"adjacency_list":adjacency_list,"num_vertices":len(vertex_index)}
+
+    return graph, profiles
+
 
 def main():
-    graph = load_fb_graph()
+    one_time_graph_processing()
+    graph, profiles = graph_processing(True)
 
 
 if __name__ == '__main__':
